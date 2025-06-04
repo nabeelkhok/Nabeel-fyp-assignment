6944ff85e328fdeb89fe769a144ce17ab202ea90
nabeelkhok <161445085+nabeelkhok@users.noreply.github.com>
2025-06-05 00:38:21 +0500

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
-import { auth } from '../firebase/config';
+import { auth } from '../../config/firebaseconfig';
 import { useNavigate, Link } from 'react-router-dom';
 
 const Login = () => {
